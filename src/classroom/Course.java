c714dd041022a6c04336039629cdb47617c545dd
Eric Karanja <ekaranja@cellulant.com>
2020-05-02 10:15:30 +0300

    Initial commit

---
+++ src/classroom/Course.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classroom;
+
+/**
+ *
+ * @author ekaranja
+ */
+public class Course {
+    
+    private String courseCode;
+    private String courseName;
+    private String faculty;
+    private int courseYear;
+    
+    
+}
---
+++ src/classroom/Student.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classroom;
+
+/**
+ *
+ * @author ekaranja
+ */
+public class Student {
+    /**
+     * Fields/properties
+     */
+    private String name;
+    private int registrationNumber;
+    private Course course;
+    private String gender;
+    private String phoneNumber;
+    private String username;
+    private String password;
+    
+    //Functions/benaviour
+    
+}
