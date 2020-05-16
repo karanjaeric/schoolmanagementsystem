d9c63f76cd549a5e5c0e3cba4a495301ef255ce3
Eric Karanja <ekaranja@cellulant.com>
2020-05-16 11:39:23 +0300

    full crud operation

--- src/schoolmanagementsystem/SchoolManagementSystem.java
+++ src/schoolmanagementsystem/SchoolManagementSystem.java
@@ -5,18 +5,13 @@
  */
 package schoolmanagementsystem;
 
-import classroom.Course;
-import classroom.Faculty;
-import classroom.Student;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Statement;
 import java.util.Scanner;
-import service.StudentService;
-import service.StudentsServiceI;
 
 /**
  *
@@ -29,9 +24,25 @@ public class SchoolManagementSystem {
      */
     public static void main(String[] args) throws SQLException {
 
+        String url = "jdbc:mysql://localhost:3306/smsdatabase?useSSL=false";
+        String dbUsername = "root";
+        String dbPassword = "";
+        String insertStudentQuery = "insert into students (name,registrationNumber,gender,course,phoneNumber,username,password)"
+                + "values (?,?,?,?,?,?,?)";
+        Connection connection = DriverManager.getConnection(url, dbUsername, dbPassword);
+
+        /**
+         * select all students
+         */
+        String selectQuery = "select * from students";
+        PreparedStatement selectPst = connection.prepareStatement(selectQuery);
+        selectPst.executeQuery();
+        ResultSet selectedRescords = selectPst.getResultSet();
+        while (selectedRescords.next()) {
+            System.out.println("Name " + selectedRescords.getString(2));
+        }
         //Scanner
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Enter Your Student Name");
         String name = scanner.nextLine();
 
@@ -58,13 +69,7 @@ public class SchoolManagementSystem {
          * insert statement create a prepared statement set parameters execute.
          *
          */
-        String url = "jdbc:mysql://localhost:3306/smsdatabase?useSSL=false";
-        String dbUsername = "root";
-        String dbPassword = "";
-        String insertStudentQuery = "insert into students (name,registrationNumber,gender,course,phoneNumber,username,password)"
-                + "values (?,?,?,?,?,?,?)";
-        Connection connection = DriverManager.getConnection(url, dbUsername, dbPassword);
-        PreparedStatement insertPst = connection.prepareStatement(insertStudentQuery);
+        PreparedStatement insertPst = connection.prepareStatement(insertStudentQuery, Statement.RETURN_GENERATED_KEYS);
         insertPst.setString(1, name);
         insertPst.setString(2, registrationNumber);
         insertPst.setString(3, gender);
@@ -74,6 +79,33 @@ public class SchoolManagementSystem {
         insertPst.setString(7, password);
 
         insertPst.executeUpdate();
+        ResultSet rs = insertPst.getGeneratedKeys();
+        if (rs.first()) {
+            System.out.println("Your Account created Successfully " + rs.getInt(1));
+        }
+
+        /**
+         * update the record after insert
+         */
+        System.out.println("Enter Id for the student to Update");
+        String id = scanner.nextLine();
+        System.out.println("Enter New Name");
+        String newName = scanner.nextLine();
+        String updateQuery = "update students set name =? where id=?";
+        PreparedStatement updatePst = connection.prepareStatement(updateQuery);
+        updatePst.setString(1, newName);
+        updatePst.setInt(2, Integer.parseInt(id));
+        updatePst.executeUpdate();
+
+        /**
+         * delete a student by ID
+         */
+        System.out.println("Enter Id for the student to Delete");
+        String deleteID = scanner.nextLine();
+        String deleteQuery = "delete from students where id=?";
+        PreparedStatement deletePst = connection.prepareStatement(deleteQuery);
+        deletePst.setInt(1, Integer.parseInt(deleteID));
+        deletePst.executeUpdate();
 
     }
 
